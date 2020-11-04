a09bcca4fc5cbe705573b59c0962ae8ac197d888
37108 <37108mail@gmail.com>
2020-11-04 09:55:26 +0900

    initial commit with settings

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: true,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'jest',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+## install
+```bash
+$ yarn add -D @37108/prettier-config
+```
+
+## setting
+edit package.json
+
+```javascript
+{
+  "eslintConfig": {
+     "extends": "@37108"
+  }
+}
+```
